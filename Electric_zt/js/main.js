cd19232a5b507db3780acc428f3a3a5fa987da85
ivanhulak <g.ivan.gulak17@gmail.com>
2022-07-14 16:01:30 +0300

    Add new project Electric_zt

---
+++ Electric_zt/js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+    $(".works__slider").slick({
+        dots: true,
+        arrows: true,
+        speed: 1000,
+        autoplaySpeed: 2000,
+        adaptiveHeight: true,
+        accessibility: false,
+    });
+
+    $('.header__burger').click(function(event){
+        $('.header__burger,.header__menu,.header__logo,.header__socials').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+})
